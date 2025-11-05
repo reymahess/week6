1749b1ce4a19dea38716a6a621e14593282b4d6f
reymahess <reynatamahessa>
2025-11-05 16:41:22 +0700

    tipe data variabel

--- tugas week 6.java
+++ tugas week 6.java
@@ -4,6 +4,7 @@ public class tugasweek6 {
 
     public static void main(String[] args) {
         Scanner go = new Scanner(System.in);
-
+        int strukturData, pemograman, basisData, algoritma, rpl;
+        
     }
 }
